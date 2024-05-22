2a8a99cb201660a8b1ccb921d817e4469577225b
diamonce <dim.chernenko@gmail.com>
2024-05-23 01:28:57 +0300

    Create Env Plugin Works!!!!!!!

--- cloudclimbers-slack-bot/config/config.go
+++ cloudclimbers-slack-bot/config/config.go
@@ -1,21 +1,12 @@
 package config
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"github.com/spf13/viper"
 	"os"
 	"path/filepath"
 )
-
-type ButtonConfig struct {
-	Text     string `mapstructure:"text"`
-	ActionID string `mapstructure:"action_id"`
-	Emoji    string `mapstructure:"emoji"`
-}
-
-type PluginConfig struct {
-	URL     string         `mapstructure:"url"`
-	Buttons []ButtonConfig `mapstructure:"buttons"`
-}
 
 type Config struct {
 	SlackBotToken string                  `mapstructure:"slack_bot_token"`
@@ -23,7 +14,32 @@ type Config struct {
 	MongoURI      string                  `mapstructure:"mongo_uri"`
 	DatabaseName  string                  `mapstructure:"database_name"`
 	Plugins       map[string]PluginConfig `mapstructure:"plugins"`
-	MainButtons   []ButtonConfig          `mapstructure:"main_buttons"`
+	MainButtons   []MainButton            `mapstructure:"main_buttons"`
+}
+
+type MainButton struct {
+	Text     string `mapstructure:"text"`
+	ActionID string `mapstructure:"action_id"`
+	Emoji    string `mapstructure:"emoji"`
+}
+
+type PluginConfig struct {
+	URL       string            `mapstructure:"url"`
+	Buttons   []ButtonConfig    `mapstructure:"buttons"`
+	Commands  string            `mapstructure:"commands"`
+	Variables map[string]string `mapstructure:"variables"`
+	Hash      HashConfig        `mapstructure:"hash"`
+}
+
+type ButtonConfig struct {
+	Text     string `mapstructure:"text"`
+	ActionID string `mapstructure:"action_id"`
+	Emoji    string `mapstructure:"emoji"`
+}
+
+type HashConfig struct {
+	Type  string `mapstructure:"type"`
+	Value string `mapstructure:"value"`
 }
 
 func LoadConfig() (*Config, error) {
@@ -32,11 +48,9 @@ func LoadConfig() (*Config, error) {
 		return nil, err
 	}
 	exeDir := filepath.Dir(exePath)
-
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(exeDir)
-
 	if err := viper.ReadInConfig(); err != nil {
 		return nil, err
 	}
@@ -46,5 +60,26 @@ func LoadConfig() (*Config, error) {
 		return nil, err
 	}
 
+	// Calculate hash for each plugin's commands and set it in the config
+	for pluginName, pluginConfig := range cfg.Plugins {
+		hash := calculateHash(pluginConfig.Commands)
+		cfg.Plugins[pluginName] = PluginConfig{
+			URL:       pluginConfig.URL,
+			Buttons:   pluginConfig.Buttons,
+			Commands:  pluginConfig.Commands,
+			Variables: pluginConfig.Variables,
+			Hash: HashConfig{
+				Type:  pluginConfig.Hash.Type,
+				Value: hash,
+			},
+		}
+	}
+
 	return &cfg, nil
+}
+
+func calculateHash(commands string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(commands))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
--- cloudclimbers-slack-bot/internal/handlers/handlers.go
+++ cloudclimbers-slack-bot/internal/handlers/handlers.go
@@ -1,48 +1,160 @@
 package handlers
 
 import (
-    "bytes"
-    "encoding/json"
-    "log"
-    "cloudclimbers-slack-bot/config"
-    "net/http"
-    "github.com/slack-go/slack"
+	"bytes"
+	"cloudclimbers-slack-bot/config"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/slack-go/slack"
+	"go.uber.org/zap"
 )
 
 type EventHandler struct {
-    plugins map[string]config.PluginConfig
+	api     *slack.Client
+	plugins map[string]config.PluginConfig
+	logger  *zap.Logger
 }
 
-func NewEventHandler(plugins map[string]config.PluginConfig) *EventHandler {
-    return &EventHandler{
-        plugins: plugins,
-    }
+func NewEventHandler(api *slack.Client, plugins map[string]config.PluginConfig, logger *zap.Logger) *EventHandler {
+	return &EventHandler{
+		api:     api,
+		plugins: plugins,
+		logger:  logger,
+	}
 }
 
 func (h *EventHandler) HandleMessageEvent(ev *slack.MessageEvent) {
-    // Assuming the action ID is included in the message text for simplicity
-    actionID := ev.Text
+	actionID := ev.Text
+	actionParts := strings.Split(actionID, "::")
+	baseActionID := actionParts[0]
 
-    var pluginURL string
-    switch actionID {
-    case "create_environment":
-        pluginURL = h.plugins["create"].URL
-    case "get_environment_status":
-        pluginURL = h.plugins["get"].URL
-    case "delete_environment":
-        pluginURL = h.plugins["delete"].URL
-    default:
-        log.Printf("Unknown action ID: %s", actionID)
-        return
-    }
+	var pluginConfig *config.PluginConfig
+	actionFound := false
 
-    payload, _ := json.Marshal(ev)
-    resp, err := http.Post(pluginURL, "application/json", bytes.NewBuffer(payload))
-    if err != nil {
-        log.Printf("Failed to call plugin: %v", err)
-        return
-    }
-    defer resp.Body.Close()
+	for _, plugin := range h.plugins {
+		for _, button := range plugin.Buttons {
+			if button.ActionID == baseActionID {
+				pluginConfig = &plugin
+				actionFound = true
+				break
+			}
+		}
+		if actionFound {
+			break
+		}
+	}
 
-    // Handle response from plugin
+	if !actionFound || pluginConfig == nil {
+		h.logger.Warn("Unknown action ID", zap.String("action_id", actionID))
+		return
+	}
+
+	// Create payload with commands, variables, and hash
+	data := map[string]interface{}{
+		"event":         ev,
+		"commands":      pluginConfig.Commands,
+		"variables":     pluginConfig.Variables,
+		"hash":          pluginConfig.Hash,
+		"sub_action_id": strings.Join(actionParts[1:], "::"),
+	}
+
+	payload, _ := json.Marshal(data)
+	resp, err := http.Post(pluginConfig.URL, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		h.logger.Error("Failed to call plugin", zap.Error(err))
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		h.logger.Error("Failed to read plugin response", zap.Error(err))
+		return
+	}
+
+	h.logger.Info("Plugin response", zap.String("response_body", string(body)))
+
+	var response map[string]interface{}
+	if err := json.Unmarshal(body, &response); err != nil {
+		h.logger.Error("Failed to parse plugin response", zap.Error(err))
+		return
+	}
+
+	messageText, ok := response["text"].(string)
+	if !ok {
+		h.logger.Warn("Plugin response missing 'text' field")
+		return
+	}
+
+	// Process blocks if they exist
+	var blocks []slack.Block
+	if blocksData, ok := response["blocks"].([]interface{}); ok {
+		blocks = make([]slack.Block, len(blocksData))
+		for i, blockData := range blocksData {
+			blockMap, ok := blockData.(map[string]interface{})
+			if !ok {
+				h.logger.Warn("Invalid block format", zap.Int("index", i))
+				continue
+			}
+
+			blockType, _ := blockMap["type"].(string)
+			h.logger.Info("Processing block", zap.String("block_type", blockType))
+			switch blockType {
+			case "section":
+				text, _ := blockMap["text"].(map[string]interface{})
+				textType, _ := text["type"].(string)
+				textContent, _ := text["text"].(string)
+				blockText := slack.NewTextBlockObject(textType, textContent, false, false)
+				section := slack.NewSectionBlock(blockText, nil, nil)
+				blocks[i] = section
+				h.logger.Info("Created section block", zap.String("text", textContent))
+			case "image":
+				imageURL, _ := blockMap["image_url"].(string)
+				altText, _ := blockMap["alt_text"].(string)
+				imageBlock := slack.NewImageBlock(imageURL, altText, "", slack.NewTextBlockObject("plain_text", altText, false, false))
+				blocks[i] = imageBlock
+				h.logger.Info("Created image block", zap.String("image_url", imageURL))
+			case "actions":
+				actionElements, ok := blockMap["elements"].([]interface{})
+				if !ok {
+					h.logger.Warn("Invalid elements format in actions block", zap.Int("index", i))
+					continue
+				}
+				actionBlocks := make([]slack.BlockElement, len(actionElements))
+				for j, action := range actionElements {
+					actionMap, ok := action.(map[string]interface{})
+					if !ok {
+						h.logger.Warn("Invalid action format in actions block", zap.Int("index", i), zap.Int("element_index", j))
+						continue
+					}
+					actionText, _ := actionMap["text"].(map[string]interface{})
+					actionTextType, _ := actionText["type"].(string)
+					actionTextContent, _ := actionText["text"].(string)
+					actionTextObject := slack.NewTextBlockObject(actionTextType, actionTextContent, false, false)
+					actionID, _ := actionMap["action_id"].(string)
+					actionBlocks[j] = slack.NewButtonBlockElement(actionID, "", actionTextObject)
+					h.logger.Info("Created button", zap.String("action_id", actionID), zap.String("text", actionTextContent))
+				}
+				blocks[i] = slack.NewActionBlock("", actionBlocks...)
+			default:
+				h.logger.Warn("Unknown block type", zap.String("block_type", blockType))
+			}
+		}
+	}
+
+	if blocks == nil {
+		// Fallback to a simple message if no blocks are defined
+		section := slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", messageText, false, false), nil, nil)
+		blocks = []slack.Block{section}
+	}
+
+	// Create and send message
+	msg := slack.MsgOptionBlocks(blocks...)
+	_, _, err = h.api.PostMessage(ev.Channel, msg)
+	if err != nil {
+		h.logger.Error("Failed to post message", zap.Error(err))
+	}
 }
